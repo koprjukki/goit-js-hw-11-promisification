11531ebd60a8b2cc4051a41dba260297917de7f3
Illya Koprjuk <illyakoprjuk@MacBook-Pro-Illya.local>
2020-04-04 00:03:46 +0300

    added hw

---
+++ js/task-3.js
@@ -0,0 +1,62 @@
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction =>
+    new Promise((resolve, reject) => {
+        const delay = randomIntegerFromInterval(200, 500);
+
+        setTimeout(() => {
+            const proccesing = Math.random() > 0.3;
+
+            if (proccesing) {
+                resolve(transaction);
+            } else {
+                reject(transaction);
+            }
+        }, delay);
+    });
+
+const logSuccess = ({
+    id,
+    amount
+}) => {
+    console.log(`Transaction ${id} processed in ${amount}ms`);
+};
+
+const logError = ({
+    id
+}) => {
+    console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Должно работать так
+ */
+makeTransaction({
+        id: 70,
+        amount: 150
+    })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({
+        id: 71,
+        amount: 230
+    })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({
+        id: 72,
+        amount: 75
+    })
+    .then(logSuccess)
+    .catch(logError);
+
+makeTransaction({
+        id: 73,
+        amount: 100
+    })
+    .then(logSuccess)
+    .catch(logError);
